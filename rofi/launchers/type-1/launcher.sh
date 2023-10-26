292b41f41d9fd3506786119dd1222de0814e7ab6
dyft4w <dyft4w@gmail.com>
2023-10-26 23:35:47 +0200

    aaa

--- rofi/launchers/type-1/launcher.sh
+++ rofi/launchers/type-1/launcher.sh
@@ -11,10 +11,9 @@
 ## style-6     style-7     style-8     style-9     style-10
 ## style-11    style-12    style-13    style-14    style-15
 
+monitor="$(swaymsg -t get\_outputs | jq -c '.\[\] | select(.focused) | select(.id)' | jq -c '.name')"
 dir="$HOME/.config/rofi/launchers/type-1"
 theme='style-7'
 
 ## Run
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+rofi -show drun -theme ${dir}/${theme}.rasi
