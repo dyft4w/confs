728ec11322bb848d09cc4f91600084cdbd988b06
dyft4w <dyft4w@gmail.com>
2024-02-21 20:14:41 +0100

    a2

--- special/install.sh
+++ special/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
+git clone https://github.com/NvChad/NvChad ~/.config/nvim
 mkdir -p ~/.config/nvim/lua
-ln -s (realpath nvchad_custom) ~/.config/nvim/lua/custom
+ln -s `realpath nvchad_custom` ~/.config/nvim/lua/custom
