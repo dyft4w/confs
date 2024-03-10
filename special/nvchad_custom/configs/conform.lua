3430ea88fa46fd968cd9770b46c2754b421646d7
Dyft4w <dyft4w@gmail.com>
2024-03-10 15:07:00 +0100

    lmao

--- special/nvchad_custom/configs/conform.lua
+++ special/nvchad_custom/configs/conform.lua
@@ -5,9 +5,10 @@ local options = {
 	formatters_by_ft = {
 		lua = { "stylua" },
 
-		javascript = { "prettier" },
-		css = { "prettier" },
-		html = { "prettier" },
+		javascript = { "prettierd" },
+		css = { "prettierd" },
+		html = { "prettierd" },
+		xhtml = { "prettierd" },
 
 		sh = { "shfmt" },
 	},
--- special/nvchad_custom/mappings.lua
+++ special/nvchad_custom/mappings.lua
@@ -2,35 +2,38 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-        require("conform").format()
-      end,
-      "formatting",
-    },
-    ["<leader>tn"] = {
-      function()
-        require("nvchad.tabufline").tabuflineNew()
-      end,
-      "Goto next buffer",
-    },
-    ["<leader>ns"] = {":<C-u>nohlsearch<cr>", ":nohlsearch", opts = { nowait = true } },
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
-  i = {
-    ["<C-a>"] = {
-    function()
-      return vim.fn['codeium#Accept']()
-    end,
-    "Codeium Accept",
-    opts = { nowait = true, silent = true, noremap = true, expr = true, script = true} },
-  },
+		--  format with conform
+
+		["<leader>tn"] = {
+			function()
+				require("nvchad.tabufline").tabuflineNew()
+			end,
+			"Goto next buffer",
+		},
+		["<leader>ns"] = { ":<C-u>nohlsearch<cr>", ":nohlsearch", opts = { nowait = true } },
+		["<leader>fm"] = {
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			"formatting",
+			opts = { expr = true, script = true, noremap = true },
+		},
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
+	i = {
+		["<C-a>"] = {
+			function()
+				return vim.fn["codeium#Accept"]()
+			end,
+			"Codeium Accept",
+			opts = { nowait = true, silent = true, noremap = true, expr = true, script = true },
+		},
+	},
 }
 
 -- more keybinds!
